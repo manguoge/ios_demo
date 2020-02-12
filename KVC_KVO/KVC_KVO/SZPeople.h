e8493a61b76ff99717b50e5bcb29612e5b0103cb
manguo <312825660@qq.com>
2020-02-12 14:17:27 +0800

    Update kvo demo for listening container property

--- KVC_KVO/KVC_KVO/SZPeople.h
+++ KVC_KVO/KVC_KVO/SZPeople.h
@@ -16,6 +16,9 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, strong) NSString *firstName;
 @property (nonatomic, strong) NSString *lastName;
 
+//容器属性
+@property (nonatomic, strong) NSMutableArray *friends;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- KVC_KVO/KVC_KVO/ViewController.m
+++ KVC_KVO/KVC_KVO/ViewController.m
@@ -21,26 +21,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor redColor];
-    
-    self.people = [SZPeople new];
-    [self.people addObserver:self forKeyPath:@"name" options:NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew context:nil];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     static int i = 0;
     i++;
+    self.people = [SZPeople new];
+    self.people.friends = [NSMutableArray array];
+//    [self.people addObserver:self forKeyPath:@"name" options:NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew context:nil];
 //    self.people.name = [NSString stringWithFormat:@"%d", i];
 //    [self.people setValue:@"1" forKey:@"name"];
 //    [self.people setValue:@"firstName" forKey:@"firstName"];//keypath 依赖
     
-//    自定义KVO
-    SZPerson *person = [SZPerson new];
-    IMP ii = [person methodForSelector:@selector(setName:)];
-    [person customAddObserver:self forKeyPath:@"name" options:NSKeyValueObservingOptionNew context:nil];
+//    对容器属性的监听
+    [self.people addObserver:self forKeyPath:@"friends" options:NSKeyValueObservingOptionNew context:nil];
+    [self.people.friends addObject:@"f1"];
     
-//    person.name = @"new name";
-    IMP iii = [person methodForSelector:@selector(setName:)];
-    [person setName:@"new name"];
+    NSMutableArray *tempFriends =  [self.people mutableArrayValueForKey:@"friends"];
+    [tempFriends addObject:@"f2"];
+    
+    
+//    自定义KVO
+//    SZPerson *person = [SZPerson new];
+//    IMP ii = [person methodForSelector:@selector(setName:)];
+//    [person customAddObserver:self forKeyPath:@"name" options:NSKeyValueObservingOptionNew context:nil];
+//
+////    person.name = @"new name";
+//    IMP iii = [person methodForSelector:@selector(setName:)];
+//    [person setName:@"new name"];
 }
 
 -(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
